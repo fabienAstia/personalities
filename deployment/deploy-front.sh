356e81989d8b8cd83f0a04d0e5bf6e573c414fef
Fabien Astiasaran <fabien.astiasaran@gmail.com>
2025-11-29 19:47:26 +0100

    Update deployment script paths to use new Jenkins agent

--- deployment/deploy-front.sh
+++ deployment/deploy-front.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 set -x
-cd "/home/jenkinsagent/agent1/workspace/personalities-front-deployment/personalities-vue"
+cd "/home/jenkins/agent1/workspace/personalities-front-deployment/personalities-vue"
 cat <<EOF> .env.production
 VITE_API_URL=https://fabien-astiasaran.com/api
 EOF
@@ -10,5 +10,5 @@ npm install
 npm run build
 rm -rf "/srv/fabien-astiasaran/www/personalities/"
 mkdir -p "/srv/fabien-astiasaran/www/personalities/"
-cp -r /home/jenkinsagent/agent1/workspace/personalities-front-deployment/personalities-vue/dist/* \
+cp -r /home/jenkins/agent1/workspace/personalities-front-deployment/personalities-vue/dist/* \
 /srv/fabien-astiasaran/www/personalities/
